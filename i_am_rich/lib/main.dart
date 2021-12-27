9377ee98c48f5b36af339d255ad2034ea82b5317
dino <sandhraprathap@gmail.com>
2021-12-28 00:08:29 +0530

    added app icon

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,15 +4,19 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.blueGrey[300],
-          appBar: AppBar(
-            backgroundColor: Colors.blueGrey[900],
-            title: Center(child: Text("I am Rich")),
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Center(
+            child: Text("I Am Rich"),
           ),
-          body: Center(
-              child: Image(
+        ),
+        body: Center(
+          child: Image(
             image: AssetImage('images/diamond.png'),
-          ))),
+          ),
+        ),
+      ),
     ),
   );
 }
