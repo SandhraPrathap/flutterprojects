328ad5650af826b935e824be67c3f132b3c906a0
dino <sandhraprathap@gmail.com>
2022-01-20 16:26:43 +0530

    Dice, an app to generate random numbers on dice

---
+++ dicee/lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftdicenum = 5;
+  var rightdicenum = 1;
+  void rand() {
+    leftdicenum = Random().nextInt(6) + 1;
+    rightdicenum = Random().nextInt(6) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rand();
+                  });
+                },
+                child: Image(
+                  image: AssetImage('images/dice$leftdicenum.png'),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rand();
+                  });
+                },
+                child:
+                    Image(image: AssetImage('images/dice$rightdicenum.png'))),
+          )),
+        ],
+      ),
+    );
+  }
+}
