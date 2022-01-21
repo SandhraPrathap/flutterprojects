5c6a3144867d3f2e2ba8bc17386f35a918ff162a
dino <sandhraprathap@gmail.com>
2022-01-21 18:09:59 +0530

    Project completed(Ask me Anything and get the answers)

--- magic8ball/lib/main.dart
+++ magic8ball/lib/main.dart
@@ -1,16 +1,48 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: BallPage(),));
+  runApp(const MaterialApp(
+    home: BallPage(),
+  ));
 }
+
 class BallPage extends StatelessWidget {
-  const BallPage({ Key? key }) : super(key: key);
+  const BallPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Ask me Anything"),
+      ),
+      body: Ball(),
     );
   }
 }
 
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int rand = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: TextButton(
+            child: Image.asset("images/ball$rand.png"),
+            onPressed: () {
+              setState(() {
+                rand = Random().nextInt(5) + 1;
+              });
+            }));
+  }
+}
