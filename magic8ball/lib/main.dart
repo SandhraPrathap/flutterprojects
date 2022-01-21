1b1139fe514081f2dfab61ef0e022f132a6cbf18
dino <sandhraprathap@gmail.com>
2022-01-21 17:31:08 +0530

    New project -magic8ball

---
+++ magic8ball/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: BallPage(),));
+}
+class BallPage extends StatelessWidget {
+  const BallPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
